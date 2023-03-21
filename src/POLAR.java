c605dff60862c7d892fb46de200d6d486b38af5a
赖海斌 <12211612@mail.sustech.edu.cn>
2023-03-21 22:47:16 +0800

    3/21 写了一个极坐标的显示程序

--- src/POLAR.java
+++ src/POLAR.java
@@ -1,4 +1,21 @@
-
-
+import java.util.Scanner;
 public class POLAR {
+    public static void main(String[] args) {
+        Scanner scan =new Scanner(System.in);
+        //String function =scan.next();
+        double n =1000;
+
+
+        for(int i=0; i < n ; i++){
+            double xita = i/n * Math.PI * 2;
+            double r = Math.cos( 0.5 * xita );
+
+
+            double x = r * Math.cos(xita);
+            double y = r * Math.sin(xita);
+            StdDraw.point(x,y);
+
+        }
+
+    }
 }
